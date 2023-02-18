a22e6802880129e341fe8950d7915e16714c5bf9
lichu <lj2231@nyu.edu>
2023-02-18 14:57:57 -0500

    1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -71,8 +71,7 @@ function Contact() {
                 Hire Me
               </h2>
               <p className="leading-relaxed mb-5">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-                suscipit officia aspernatur veritatis. Asperiores, aliquid?
+              I have always embraced change and sought opportunities within it.
               </p>
               <div className="relative mb-4">
                 <label htmlFor="name" className="leading-7 text-sm text-gray-400">
