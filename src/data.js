d1a8a2320083c11e56b97683c36dbd6adef0fd3a
lichu <lj2231@nyu.edu>
2023-02-26 15:27:53 -0500

    revise

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const projects = [
         subtitle: "Java Fullstack",
         description: "I have developed a multiplayer online Snake game that supports at least 200 players simultaneously. The backend service is built with Spring Boot, and it offers a wide range of functionalities, including user registration and login based on JWT, microservice architecture implemented with Spring Cloud, player matching service using RabbitMQ message queue, support for players to write their own Snake scripts for human-vs-machine or machine-vs-machine battles in real-time (using WebSockets), and a replay feature for reviewing past matches. The frontend is developed with Vue, and the leaderboard is powered by Redis as a cache, which boosts read performance by 10%. Finally, I have deployed the application to AWS using Docker and Nginx.",
         image: "./PK.gif",
-        link: "https://app4477.acapp.acwing.com.cn/"
+        link: "https://app4561.acapp.acwing.com.cn/"
     },
 ];
 
